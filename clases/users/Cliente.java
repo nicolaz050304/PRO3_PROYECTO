f76959cdc6086342e2aae37628eccc9b27310f70
Nicolaz <a20234964@pucp.edu.pe>
2026-04-04 06:07:33 -0500

    cambios en CLiente pase de List a ArrayList para mejorar la felxibilidad

--- clases/users/Cliente.java
+++ clases/users/Cliente.java
@@ -4,10 +4,10 @@ public class Cliente extends Usuario{
 	private String nombre;
 	private String telefono;
 	private String dni;
-	private ArrayList<Reservas> reservas;//lista de reservas, aqui estan las pasadas y nuevas tambien xd
+	private List<Reservas> reservas;//lista de reservas, aqui estan las pasadas y nuevas tambien xd
 	public Cliente(String username,String correo,String contraseña,
 					String nombre,String telefono,String dni){
-		super(username,correo,contraseña);
+		super(username,correo,contraseña);	
 		this.nombre=nombre;
 		this.telefono=telefono;
 		this.dni=dni;
