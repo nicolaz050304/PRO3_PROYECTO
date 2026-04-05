b48e65df1c4df7a45fb9faeff24cfeeceeb3c63b
ArianAlaniaGIT <arianalfonso040207@gmail.com>
2026-04-04 21:49:38 -0500

    hello

---
+++ clases/users/Propietario.java
@@ -0,0 +1,12 @@
+package users;
+
+public class Propietario extends Usuario {
+	private double calificacionPromedio; // reputacion del propietario basada en reseñas
+	private String cuentaBancaria; // donde va a recibir los pagos de los clientes
+	
+	public Propietario(String username,String correo,String contraseña, String cuentaBancaria, double calificacionPromedio) {
+		super(username,correo,contraseña);
+		this.calificacionPromedio = calificacionPromedio;
+		this.cuentaBancaria = cuentaBancaria;
+	}
+}
