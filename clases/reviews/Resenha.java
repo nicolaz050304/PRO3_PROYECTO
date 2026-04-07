d7412dc292c05acf1250d813b9abdbc567ba36b4
Draco-estefan <aguilar.estefano@pucp.edu.pe>
2026-04-07 17:25:29 -0500

    Update Resenha.java

--- clases/reviews/Resenha.java
+++ clases/reviews/Resenha.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 public class Resenha {
 
-    // --- Atributos ---
+    // --- Atributos
     private int idResenha;
     private Cliente autor;
     private Alojamiento alojamiento;
