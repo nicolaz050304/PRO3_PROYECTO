8d20e037f5ea4cc2799ddf35a97d9ccd5b8b7f22
Draco-estefan <aguilar.estefano@pucp.edu.pe>
2026-04-07 17:22:43 -0500

    Add files via upload

---
+++ clases/Resenha.java
@@ -0,0 +1,78 @@
+package reviews;
+
+import users.Cliente;
+import accommodations.Alojamiento; 
+import java.util.Date;
+
+public class Resenha {
+
+    // --- Atributos ---
+    private int idResenha;
+    private Cliente autor;
+    private Alojamiento alojamiento;
+    private int calificacion;
+    private String comentario;
+    private Date fechaPublicacion;
+
+    public Resenha() {
+    }
+
+    public Resenha(int idResenha, Cliente autor, Alojamiento alojamiento, int calificacion, String comentario, Date fechaPublicacion) {
+        this.idResenha = idResenha;
+        this.autor = autor;
+        this.alojamiento = alojamiento;
+        this.calificacion = calificacion;
+        this.comentario = comentario;
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    // --- Getters y Setters 
+    public int getIdResenha() { 
+		return idResenha; 
+	}
+    public void setIdResenha(int idResenha) {
+		this.idResenha = idResenha; 
+	}
+
+    public Cliente getAutor() { 
+	return autor; 
+	}
+    public void setAutor(Cliente autor) {
+		this.autor = autor; 
+	}
+
+    public Alojamiento getAlojamiento() { 
+	return alojamiento; 
+	}
+    public void setAlojamiento(Alojamiento alojamiento) {
+		this.alojamiento = alojamiento; 
+	}
+
+    public int getCalificacion() { 
+	return calificacion; 
+	}
+    public void setCalificacion(int calificacion) {
+		this.calificacion = calificacion;
+	}
+
+    public String getComentario() {
+		return comentario; 
+	}
+    public void setComentario(String comentario) {
+		this.comentario = comentario; 
+	}
+
+    public Date getFechaPublicacion() { 
+		return fechaPublicacion;
+	}
+    public void setFechaPublicacion(Date fechaPublicacion) {
+		this.fechaPublicacion = fechaPublicacion;
+	}
+
+    // --- Metodos -> solo estan declarados
+    public void editarResenha(String nuevoComentario, int nuevaCalificacion) {
+    }
+
+    public void eliminarResenha() {
+    }
+}
